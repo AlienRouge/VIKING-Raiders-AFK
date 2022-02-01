28607fe92ca07ae53e5e7ee5bbc386907d29b8aa
Vladislav <rudov.vlad2013@gmail.com>
2022-02-01 16:35:38 +0700

    Refactoring and Simple target finding

--- VIKING-Raiders-AFK/Assets/Scripts/PlayerSpawnPoint.cs
+++ VIKING-Raiders-AFK/Assets/Scripts/PlayerSpawnPoint.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerSpawnPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform[] GetSpawnTransforms()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var transforms = GetComponentsInChildren<Transform>();
+        return transforms.Skip(1).ToArray();
     }
 }
